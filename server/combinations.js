595befc57c18b2ed77623e5c457c0886674ee20d
OpenShift guest <nickkell@gmail.com>
2014-01-12 15:00:24 -0500

    refactored the combinations code to reduce duplication

--- server/combinations.js
+++ server/combinations.js
@@ -1,48 +1,39 @@
 //calling combinations() returns an object with a method called all
 function combinations() {
     
+    var replacement = function(original) {
+        return function(toReplace, replaceWith) {
+            return original.replace(toReplace, replaceWith);
+        };
+    };
+    
     return {
         //This is a method that takes a single parameter (a string called initialTerm), 
         //that the user searched for. It returns an array of permutations of the search
         //term that they may also be interested in
         all: function(initialTerm) {
-            //This is a local variable called 'x' that represents an array. Variables declared in functions
-            //are only visible within the function, i.e. within the two braces ('{' and '}')
-            var x = [];
             
-            //Push is an array method that pushes an item into the next available slot
-            x.push(initialTerm);
+            var replacementForOriginal = replacement(initialTerm),
             
-            var replaceAA = initialTerm.replace("aa", "a");
-            x.push(replaceAA);
-            
-            var replaceOO = initialTerm.replace("oo", "o");
-            x.push(replaceOO);
-            
-            var replaceEE = initialTerm.replace("ee", "e");
-            x.push(replaceEE);
-            
-            var replaceUV = initialTerm.replace("u", "v");
-            x.push(replaceUV);
-            
-            var replaceUW = initialTerm.replace("u", "w");
-            x.push(replaceUW);
-            
-            var replaceVU = initialTerm.replace("v", "u");
-            x.push(replaceVU);
-            
-            var replaceVW = initialTerm.replace("v", "w");
-            x.push(replaceVW);
-            
-            var replaceWV = initialTerm.replace("w", "v");
-            x.push(replaceWV);
-            
-            var replaceWU = initialTerm.replace("w", "u");
-            x.push(replaceWU);
-            
-            //Functions always return a value - if the 'return' keyword is not used then it returns 'undefined',
-            //a special JavaScript value. 
-            return x;//an array of all search terms
+                allPermuations = _.map([
+                    ['',''],
+                    ['aa', 'a'],
+                    ['a', 'aa'],
+                    ['ee', 'e'],
+                    ['e', 'ee'],
+                    ['oo', 'o'],
+                    ['o', 'oo'],
+                    ['u', 'v'],
+                    ['u', 'w'],
+                    ['w', 'u'],
+                    ['w', 'v'],
+                    ['v', 'u'],
+                    ['v', 'w']
+                ], function(applicationOfReplacement) {
+                    return replacementForOriginal(applicationOfReplacement[0], applicationOfReplacement[1]);
+                });
+                
+            return allPermuations;
         }
     };
     
